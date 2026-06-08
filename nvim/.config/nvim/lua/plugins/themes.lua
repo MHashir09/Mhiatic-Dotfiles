366d90738d93ce3d97351b50539a6831cedf824b
MHashir09 <mhia09@proton.me>
2026-06-09 01:10:31 +0500

    Chore: Remove nvim directory
    - Because the nvim configuration is transferred to a separate repository
      at: https://github.com/MHashir09/Meovim

--- nvim/.config/nvim/lua/plugins/themes.lua
+++
@@ -1,8 +0,0 @@
--- This uses a custom theme defined in nvim/colors/custom.lua
-
-return {
-  dir = "~/.config/nvim/colors",
-  config = function()
-    vim.cmd.colorscheme("Mhiatic-monochrome")
-  end,
-}
